ab12e2abea82279274a348fe657c9b8e9872c948
Senexys <timoviveen@gmail.com>
2019-07-27 14:45:48 +0200

    Replace && with ;.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 NOW=$(date +"%F")
 cd ~/Google-Weather-Frog
-{ node app.js && git add $(find . -size +0c) && git commit -m "Automatic image upload." && git push; } >$NOW.log 2>&1
+{ node app.js; git add $(find . -size +0c); git commit -m "Automatic image upload."; git push; } >$NOW.log 2>&1
