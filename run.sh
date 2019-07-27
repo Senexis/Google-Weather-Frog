38f58a6b03f944190770b776825ef11982fe7079
Senexys <timoviveen@gmail.com>
2019-07-27 14:07:26 +0200

    Update batch script and add .log to gitignore.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-now=`date +"%m_%d_%Y"`
+NOW=$(date +"%F")
 cd ~/Google-Weather-Frog
-{ node app.js && git add $(find . -size +0c) && git commit -m "Automatic image upload." && git push; } >"log_$now.txt" 2>&1
+{ node app.js && git add $(find . -size +0c) && git commit -m "Automatic image upload." && git push; } >$NOW.log 2>&1
