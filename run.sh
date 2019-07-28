bc31c38f99baf902e9fc43a48789648b70a8c794
Senexis <timoviveen@gmail.com>
2019-07-28 19:46:03 +0200

    Update run.sh.

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 NOW=$(date +"%F")
 cd ~/Google-Weather-Frog
 { node app.js; git add $(find . -size +0c); git commit -m "Automatic image upload."; git push; } >$NOW.log 2>&1
+cd /website
+{ node generate.js; cd ..; git add $(find . -size +0c); git commit -m "Automatic website update."; git push; }
