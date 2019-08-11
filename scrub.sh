23fde599e4805151670013d3909bb3b484efc570
Senexys <timoviveen@gmail.com>
2019-08-11 21:45:51 +0200

    Fix scripts.

--- scrub.sh
+++ scrub.sh
@@ -1,5 +1,2 @@
-NOW=$(date +"%F")
-cd ~/Google-Weather-Frog
-{ node scrub.js; git add $(find . -size +0c); git commit -m "Automatic image upload."; git push; } >$NOW.log 2>&1
-cd ./website
-{ node generate.js; cd ..; git add $(find . -size +0c); git commit -m "Automatic website update."; git push; }
+{ node scrub.js; git add $(find . -size +0c); git commit -m "Automatic image upload."; git push; } > "$(date +"%Y-%m-%d_%H-%M").log" 2>&1 &
+{ node /website/generate.js; git add $(find . -size +0c); git commit -m "Automatic website update."; git push; }
