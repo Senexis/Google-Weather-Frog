2c06599f674d9824f8050ed5f079610bf2fce667
Senexis <timoviveen@gmail.com>
2019-09-03 14:18:10 +0000

    Fix relative paths for bash.

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 { node sync.js; git add $(find . -size +0c); git commit -m "Automatic image upload."; git push; } > "$(date +"%Y-%m-%d_%H-%M").log" 2>&1 &
-{ node /website/generate.js; git add $(find . -size +0c); git commit -m "Automatic website update."; git push; }
+{ node website/generate.js; git add $(find . -size +0c); git commit -m "Automatic website update."; git push; }
