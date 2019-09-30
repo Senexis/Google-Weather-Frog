c2bd0c27ddb81e50fcbf43c0bd2342dc0d9357e5
Senexis <timoviveen@gmail.com>
2019-09-30 20:14:32 +0000

    Update bash scripts to log to logs folder.

--- scrub.sh
+++ scrub.sh
@@ -1,1 +1,1 @@
-{ node scrub.js; git add $(find . -size +0c); git commit -m "Automatic image upload."; git push; } > "scrub-$(date +"%Y-%m-%d_%H-%M").log" 2>&1
+{ node scrub.js; git add $(find . -size +0c); git commit -m "Automatic image upload."; git push; } > "logs/scrub-$(date +"%Y-%m-%d_%H-%M").log" 2>&1
--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-{ node sync.js; git add $(find . -size +0c); git commit -m "Automatic image upload."; git push; } > "sync-$(date +"%Y-%m-%d_%H-%M").log" 2>&1
+{ node sync.js; git add $(find . -size +0c); git commit -m "Automatic image upload."; git push; } > "logs/sync-$(date +"%Y-%m-%d_%H-%M").log" 2>&1
