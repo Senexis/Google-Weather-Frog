184f7108d6b6ce9c1c2f389fb686f48742dd5e59
Senexis <timoviveen@gmail.com>
2019-09-30 20:06:09 +0000

    Update location weather.

--- weather.sh
+++ weather.sh
@@ -1,1 +1,1 @@
-{ node weather.js; git add $(find . -size +0c); git commit -m "Update location weather."; git push; } > "weather-$(date +"%Y-%m-%d_%H-%M").log" 2>&1
+node weather.js; git add $(find . -size +0c); git commit -m "Update location weather."; git push;
