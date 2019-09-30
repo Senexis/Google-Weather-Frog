98d26716f8d0a938a867c1079c175e0cdcaaa1b0
Senexis <timoviveen@gmail.com>
2019-09-30 20:12:49 +0000

    Automatic website update.

--- website.sh
+++ website.sh
@@ -1,1 +1,1 @@
-{ cd website; node generate.js; cd ..; git add $(find . -size +0c); git commit -m "Automatic website update."; git push; } > "website-$(date +"%Y-%m-%d_%H-%M").log" 2>&1
+{ cd website/; node generate.js; cd ..; git add $(find . -size +0c); git commit -m "Automatic website update."; git push; } > "logs/website-$(date +"%Y-%m-%d_%H-%M").log" 2>&1
